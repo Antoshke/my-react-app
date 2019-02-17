a73cb1e69338188d02ddd7c9fd1f5ecee26333a4
Anton Glukhov <aaglukhov@gmail.com>
2019-02-18 00:09:37 +0300

    Link title changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React app</h1>
     <p>Complete courses about the the best tool and design systems. Prototype and build app with React and Swift.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
